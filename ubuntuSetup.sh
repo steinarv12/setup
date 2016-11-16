f29bb6491b672b4e2d11c4ababdf68341480b7d9
Steinar <steinarv12@ru.is>
2016-11-16 19:48:33 +0000

    Redesign

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 
+# Extra programs
+
+apt-get install git -y
+apt-get install vim -y
+apt-get install curl -y
+
 # Invert script
 
-apt-get install xcalib
-echo "alias invert='xcalib -i -a'" >> ~/.bash_aliases
+sudo apt-get install libxcb-randr0-dev
+
+cd
+cd Documents
+mkdir gits
+cd gits
+git clone https://github.com/zoltanp/xrandr-invert-colors.git
+cd xrandr-invert-colors
+sudo make deps-apt
+make
+sudo make install
+cd
+
+# Look
+
+## Plank dock
+sudo add-apt-repository ppa:ricotz/docky #
+sudo apt-get update
+sudo apt-get install plank
+## plank --preferences for preferences
+
+## Wallpaper - needs more work
+cd Pictures
+mkdir Wallpapers
+cd Wallpapers
+wget https://i.imgur.com/iX6hwvx.jpg
+cd
 
 # Sublime
 
 add-apt-repository ppa:webupd8team/sublime-text-3
 apt-get update
 apt-get install sublime-text-installer -y
-apt-get install vim -y
-apt-get install curl -y
 
-# Node and NPM
-apt-get install npm -y
-npm cache clean -f
-npm install -g n
-n stable
-npm install -g bower
-npm install -g grunt
-# sudo ln -sf /usr/local/n/version/node/<vers>/bin/node /usr/bin/nodejs
+# Spotify
 
-# Python related
-apt-get install pip -y
-apt-get install pip3 -y
+## 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+## 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+## 3. Update list of available packages
+sudo apt-get update
+## 4. Install Spotify
+sudo apt-get install spotify-client
